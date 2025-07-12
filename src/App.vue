c196cc8b250e0b0e3d1efb45b58367cf00725ec8
Alexandre Andrade <aleandrade@gmail.com>
2025-07-12 15:12:09 +1000

    fix: 🐛 Overflow

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 <template>
       <div class="a0-container bg-[--hl-xs] notice surprise">
       <img class="a0-logo" src="/home/maat/Development/js/auth0/insomnia-auth0/images/icon.svg" />
-      <header class="a0-header border-b border-solid bg-[--color-bg]">
-        <div>Domains</div>
+      <header class="a0-header border-b border-solid bg-[--color-bg] text-[--hl]">
+        <div>Domain</div>
         <div>Status</div>
         <div>Action</div>
       </header>
