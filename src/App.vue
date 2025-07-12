4c908633536361ffac532adb22b90e9545afb02a
Alexandre Andrade <aleandrade@gmail.com>
2025-07-12 10:00:27 +1000

    style: 💄 Adapt to Insomnia colour scheme

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <script setup>
-import { ref } from 'vue'
-
-const msg = ref('Hello World!')
 </script>
 
 <template>
-  <div class="a0-container">
-    <img class="a0-logo" src="images/icon.svg" />
-    <header class="a0-header">
-      <div>Domains</div>
-      <div>Status</div>
-      <div>Action</div>
-    </header>
-    <div class="a0-instance">
-      <div>Domains</div>
-      <div>Status</div>
-      <div>Action</div>
+      <div class="a0-container bg-[--hl-xs] notice surprise">
+      <img class="a0-logo" src="/home/maat/Development/js/auth0/insomnia-auth0/images/icon.svg" />
+      <header class="a0-header border-b border-solid bg-[--color-bg]">
+        <div>Domains</div>
+        <div>Status</div>
+        <div>Action</div>
+      </header>
+      <div class="a0-instance notice info">
+        <div>Domains</div>
+        <div>Status</div>
+        <div>Action</div>
+      </div>
     </div>
-  </div>
 </template>
