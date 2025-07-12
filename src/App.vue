08adb806cd65cbaf1b90d44a2ecfc4af75e4813e
Alexandre Andrade <aleandrade@gmail.com>
2025-07-13 00:10:49 +1000

    feat: 🎸 Integrate auth0Instances

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script setup>
+import { ref } from 'vue'
+import Instance from './Instance.vue'
+const auth0Instances = ref(auth0.auth0Instances)
 </script>
 
 <template>
-      <div class="a0-container bg-[--hl-xs] notice surprise">
-      <img class="a0-logo" src="/home/maat/Development/js/auth0/insomnia-auth0/images/icon.svg" />
+  <div class="a0-container bg-[--hl-xs] notice surprise">
+    <img class="a0-logo" src="/home/maat/Development/js/auth0/insomnia-auth0/images/icon.svg" />
+    <div v-if="auth0Instances.length > 0">
       <header class="a0-header border-b border-solid bg-[--color-bg] text-[--hl]">
         <div>Domain</div>
         <div>Status</div>
         <div>Action</div>
       </header>
-      <div class="a0-instance notice info">
-        <div>Domains</div>
-        <div>Status</div>
-        <div>Action</div>
+      <div v-for="instance in auth0Instances">
+        <Suspense>
+          <Instance :instance="instance" />
+          <template #fallback>
+            <div>Loading...</div>
+          </template>
+        </Suspense>
       </div>
     </div>
+    <div v-else class="a0-header">
+      <h1>No Auth0 plugin configuration found.</h1>
+    </div>
+  </div>
 </template>
