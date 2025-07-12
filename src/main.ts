19a525c4ccf86d29613ddf7eb40f7b5b09b0a4eb
Alexandre Andrade <aleandrade@gmail.com>
2025-07-12 15:07:16 +1000

    feat: 🎸 Populate auth0Instances before UI

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { Auth0 } from './auth0'
 import type { Context } from './types'
 import createVue from './vue'
+import { warn } from './logger'
 
 const auth0 = new Auth0()
+globalThis.auth0 = auth0
 
 const requestHooks = [auth0.applyHook]
 
 const workspaceActions = [
   {
-    label: 'Auth0 Login',
+    label: 'Auth0 Login/Logout',
     icon: 'shield-halved',
-    action: async (context: Context) => {
+    action: async (context: Context, models: any) => {
+      if (auth0.auth0Instances.length === 0) {
+        warn('Not initialized: forcing.. ')
+        auth0.forceInitialization()
+        const localRequest = models.requests.find((req) => req.url.includes('localhost'))
+        const req = localRequest || models.requests[0]
+        if (req) await context.network.sendRequest(req)
+      }
       const vue = createVue()
       context.app.dialog('Auth0', vue, { tall: true, wide: true })
     }
