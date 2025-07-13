14b07994854ebe8df4cdf7717df328d243b82145
Alexandre Andrade <aleandrade@gmail.com>
2025-07-14 00:22:38 +1000

    style: 💄 Improve button

--- src/ButtonAuth.vue
+++ src/ButtonAuth.vue
@@ -27,7 +27,7 @@ const click = () => {
 
 <template>
     <button @click="click" type="button" tabindex="0"
-        class="border border-solid border-[--hl-md] bg-[rgba(var(--color-success-rgb),var(--tw-bg-opacity))] bg-opacity-100 font-semibold text-[--color-font-success] flex h-full items-center justify-center gap-2 rounded-md px-4 py-2 text-sm ring-1 ring-transparent transition-all hover:bg-opacity-80 focus:ring-inset focus:ring-[--hl-md] aria-pressed:opacity-80"
+        class="btn btn--clicky mt-[--padding-md]"
         data-rac="" id="react-aria7835462272-:r24:" style="touch-action: pan-x pan-y pinch-zoom;"
         v-html="elementChildren">
     </button>
