abd560d02d58da383cd60d805a9e3fc2b1552f35
Alexandre Andrade <aleandrade@gmail.com>
2025-07-13 19:27:09 +1000

    style: 💄 Adjust layout

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,16 @@ const auth0Instances = ref(auth0?.auth0Instances)
     <img class="a0-logo" :src="imgSrc" />
     <div v-if="auth0Instances?.length > 0">
       <header class="a0-header border-b border-solid bg-[--color-bg] text-[--hl]">
-        <div>Domain</div>
-        <div>Status</div>
-        <div>Action</div>
+        <div style="flex: 2;">Domain</div>
+        <div style="flex: 1;">Status</div>
+        <div style="flex: 2;">Action</div>
       </header>
-      <div v-for="(instance, index) in auth0Instances">
         <Suspense>
-          <Instance :instance :id="index" />
+          <Instance v-for="(instance, index) in auth0Instances" :instance :id="index" />
           <template #fallback>
             <div>Loading...</div>
           </template>
         </Suspense>
-      </div>
     </div>
     <div v-else class="a0-header">
       <h1>No Auth0 plugin configuration found.</h1>
