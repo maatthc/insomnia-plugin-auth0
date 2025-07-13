599416b031b480abc98a141c9ad15c1585b8fce6
Alexandre Andrade <aleandrade@gmail.com>
2025-07-13 11:28:28 +1000

    feat: 🎸 Add ButtonAuth

---
+++ src/ButtonAuth.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { computed, toValue } from 'vue'
+import fingerPrint from '../images/fingerprint.svg'
+import logOut from '../images/logout.svg'
+
+const props = defineProps({
+    authenticated: {
+        type: Boolean,
+        require: true
+    }
+})
+
+const elementChildren = computed(() => {
+    const authenticated = toValue(props.authenticated)
+    const icon = authenticated ? logOut : fingerPrint
+    const text = authenticated ? 'Logout' : 'Authenticate'
+    return `${icon}\n<span class="truncate">${text}</span>`
+})
+</script>
+
+<template>
+    <button type="button" tabindex="0"
+        class="border border-solid border-[--hl-md] bg-[rgba(var(--color-success-rgb),var(--tw-bg-opacity))] bg-opacity-100 font-semibold text-[--color-font-success] flex h-full items-center justify-center gap-2 rounded-md px-4 py-2 text-sm ring-1 ring-transparent transition-all hover:bg-opacity-80 focus:ring-inset focus:ring-[--hl-md] aria-pressed:opacity-80"
+        data-rac="" id="react-aria7835462272-:r24:" style="touch-action: pan-x pan-y pinch-zoom;"
+        v-html="elementChildren">
+    </button>
+</template>
